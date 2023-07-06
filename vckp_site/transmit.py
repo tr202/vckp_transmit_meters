e7d91b648217ab91f586f4ef39fe92ad038a7b90
Marid <marid@marid.Dlink>
2023-07-06 23:50:34 +0300

    tt

---
+++ vckp_site/transmit.py
@@ -0,0 +1,30 @@
+import undetected_chromedriver as uc
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class Main:
+  def __init_(self) -> None:
+    self.url    = 'https://ikus.pesc.ru/auth/login'
+    self.driver = driver = uc.Chrome(use_subprocess=True)
+    self.time   = 10
+    
+  def login(self, email, password):
+    # edit: fixed missing end-quotes on below lines
+    WebDriverWait(self.driver, 20).until(EC.visibility_of_element_located((By.NAME, 'identifier'))).send_keys(f'{email}\n')
+    WebDriverWait(self.driver, 20).until(EC.visibility_of_element_located((By.NAME, 'password'))).send_keys(f'{password}\n')
+                                                                                
+    self.code()
+                                                                                  
+  def code(self):
+    # [ ---------- paste your code here ---------- ]
+    time.sleep(self.time)                                                                                  
+                                                                                  
+if __name__ == "__main__":
+  #  ---------- EDIT ----------
+  email = 'email' # replace email
+  password = 'password' # replace password
+  #  ---------- EDIT ----------                                                                                                                                                         
+ 
+  driver = Main()
+  driver.login(email, password) 
